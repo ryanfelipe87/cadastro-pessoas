b6fe856537f2ac5fc5954daa67bd679e298d080b
Ryan <ryanfelipe.rr@gmail.com>
2024-03-12 16:23:14 -0300

    Database configuration

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Person (id, name, age, gender) VALUES (1, "Cristiano", 39, "Masculino");
+INSERT INTO Person (id, name, age, gender) VALUES (2, "Messi", 36, "Masculino");
+INSERT INTO Person (id, name, age, gender) VALUES (3, "Neymar", 32, "Masculino");
+INSERT INTO Person (id, name, age, gender) VALUES (4, "Bellingham", 20, "Masculino");
+INSERT INTO Person (id, name, age, gender) VALUES (5, "Marta", 38, "Feminina");
+
+INSERT INTO Address(id, state, city, publicPlace, number, cep) VALUES (1, "Vila", "Ilha da madeira", "Portugal", 303, 98522-420);
+INSERT INTO Address(id, state, city, publicPlace, number, cep) VALUES (2, "Comunidade", "Rosario", "Argentina", 70, 55423-020);
+INSERT INTO Address(id, state, city, publicPlace, number, cep) VALUES (3, "Santos", "Zona Oeste", "Brasil", 21, 33500-198);
+INSERT INTO Address(id, state, city, publicPlace, number, cep) VALUES (4, "Kansas City", "Liverpool", "Inglaterra", 914, 25418-540);
+INSERT INTO Address(id, state, city, publicPlace, number, cep) VALUES (5, "Morrinho", "Alagoas", "Brasil", 91, 96582-478);
