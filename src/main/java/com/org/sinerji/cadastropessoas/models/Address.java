57bf3e4f594cb1aa03072b6cfed1abc31c1a7add
Ryan <ryanfelipe.rr@gmail.com>
2024-03-12 15:18:46 -0300

    Models complete

--- src/main/java/com/org/sinerji/cadastropessoas/models/Address.java
+++ src/main/java/com/org/sinerji/cadastropessoas/models/Address.java
@@ -1,4 +1,88 @@
 package com.org.sinerji.cadastropessoas.models;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String state;
+    private String city;
+    private String publicPlace;
+    private int number;
+    private String cep;
+
+    @ManyToOne
+    private Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPublicPlace() {
+        return publicPlace;
+    }
+
+    public void setPublicPlace(String publicPlace) {
+        this.publicPlace = publicPlace;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return number == address.number && Objects.equals(id, address.id) && Objects.equals(state, address.state) && Objects.equals(city, address.city) && Objects.equals(publicPlace, address.publicPlace) && Objects.equals(cep, address.cep) && Objects.equals(person, address.person);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, state, city, publicPlace, number, cep, person);
+    }
 }
--- src/main/java/com/org/sinerji/cadastropessoas/models/Person.java
+++ src/main/java/com/org/sinerji/cadastropessoas/models/Person.java
@@ -1,6 +1,8 @@
 package com.org.sinerji.cadastropessoas.models;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Person {
@@ -14,4 +16,58 @@ public class Person {
 
     @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
     private List<Address> addresses;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(id, person.id) && Objects.equals(name, person.name) && Objects.equals(gender, person.gender) && Objects.equals(addresses, person.addresses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, gender, addresses);
+    }
 }
